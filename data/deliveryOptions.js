1a100042dd21af12b14d251af86b7ac44a17bb99
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-03-06 22:36:26 +0530

    add isSatSun function

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -28,8 +28,26 @@ export function getDeliveryOption(deliveryOptionId){
     return deliveryOption;
 };
 
+function isWeekend(date){
+    const dayOfWeek = date.format('dddd');
+    return dayOfWeek === 'Sunday' || dayOfWeek === 'Saturday'
+}
+
 export function calculateDeliveryDate(deliveryOption){
-    const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryTime,'days');
+    // const today = dayjs();
+    // const deliveryDate = today.add(deliveryOption.deliveryTime,'days');
+
+
+    let remainingDays = deliveryOption.deliveryTime;
+    let deliveryDate = dayjs();
+
+    while(remainingDays > 0){
+        deliveryDate = deliveryDate.add(1,'day');
+
+        if (!isWeekend(deliveryDate)){
+            remainingDays-- ;
+        }
+    }
+
     return deliveryDate.format('dddd MMM D');
 };
