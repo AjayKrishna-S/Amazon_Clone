9ba9cc13b230e984016b2b40848f7c20651bba91
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-03-22 15:09:50 +0530

    update

--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -2,6 +2,8 @@ import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
 
 describe('test suit: addToCart',()=>{
     it('adds an new product to cart',()=>{
+        spyOn(localStorage, 'setItem');
+
         spyOn(localStorage,'getItem').and.callFake(()=>{
             return JSON.stringify([]);
         });
@@ -10,5 +12,6 @@ describe('test suit: addToCart',()=>{
         
         addToCart('15b6fc6f-327a-4ec4-896f-486349e85a3d','1');
         expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     })
 })
